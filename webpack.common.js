35a65e24f8ef509b63e3babe98d878807bdd44d5
mart1gas <angmor.zuev@yandex.ru>
2021-09-19 01:37:03 +0300

    add favicon.ico in root public folder, to plug favicon -  HtmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      favicon: './public/favicon.ico',
     }),
     new StylelintPlugin({
       configFile: './.stylelintrc.json',
